4645d3990108c45c3c1ef51712fa2591ed4f3c80
Syril <syrilster@gmail.com>
2020-06-02 11:47:04 +1000

    Added more unit tests

--- internal/service_test.go
+++ internal/service_test.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -31,12 +32,74 @@ func TestLeaveMigration(t *testing.T) {
 		TypeOfUnits:   "Hours",
 	}
 
+	tenantID := "111111"
+	empID := "123456"
+	var connectionResp = []xero.Connection{
+		{
+			TenantID:   tenantID,
+			TenantType: "Org",
+			OrgName:    "DigIO",
+		},
+		{
+			TenantID:   tenantID,
+			TenantType: "Org",
+			OrgName:    "Mantel Group",
+		},
+		{
+			TenantID:   tenantID,
+			TenantType: "Org",
+			OrgName:    "CMD",
+		},
+		{
+			TenantID:   tenantID,
+			TenantType: "Org",
+			OrgName:    "Eliiza",
+		},
+	}
+
+	empResp := &xero.EmpResponse{
+		Status: "Active",
+		Employees: []xero.Employee{
+			{
+				EmployeeID:        empID,
+				FirstName:         "Syril",
+				LastName:          "Sadasivan",
+				Status:            "Active",
+				PayrollCalendarID: "4567891011",
+				LeaveBalance: []xero.LeaveBalance{
+					annualLeave,
+					personalLeave,
+				},
+			},
+		},
+	}
+
+	payRollCalendarResp := &xero.PayrollCalendarResponse{
+		PayrollCalendars: []xero.PayrollCalendar{
+			{
+				PayrollCalendarID: "4567891011",
+				PaymentDate:       "/Date(632102400000+0000)/",
+			},
+		},
+	}
+
+	leaveBalResp := &xero.LeaveBalanceResponse{Employees: empResp.Employees}
+
 	t.Run("Success", func(t *testing.T) {
-		_, b, _, _ := runtime.Caller(0)
-		basePath := filepath.Dir(b)
-		dir := path.Join(path.Dir(basePath), ".")
-		tenantID := "111111"
-		empID := "123456"
+		mockClient := new(MockXeroClient)
+		mockClient.On("GetConnections", context.Background()).Return(connectionResp, nil)
+		mockClient.On("GetEmployees", context.Background(), tenantID).Return(empResp, nil)
+		mockClient.On("GetPayrollCalendars", context.Background(), tenantID).Return(payRollCalendarResp, nil)
+		mockClient.On("EmployeeLeaveBalance", context.Background(), tenantID, empID).Return(leaveBalResp, nil)
+		mockClient.On("EmployeeLeaveApplication", context.Background(), tenantID, mock.Anything).Return(nil)
+		xlsLocation := getProjectRoot() + "/test/digioLeave.xlsx"
+		service := NewService(mockClient, xlsLocation)
+
+		err := service.MigrateLeaveKrowToXero(context.Background())
+		assert.Nil(t, err)
+	})
+
+	t.Run("Error when ORG is missing in Xero", func(t *testing.T) {
 		var connectionResp = []xero.Connection{
 			{
 				TenantID:   tenantID,
@@ -44,6 +107,24 @@ func TestLeaveMigration(t *testing.T) {
 				OrgName:    "DigIO",
 			},
 		}
+
+		mockClient := new(MockXeroClient)
+		mockClient.On("GetConnections", context.Background()).Return(connectionResp, nil)
+		mockClient.On("GetEmployees", context.Background(), tenantID).Return(empResp, nil)
+		mockClient.On("GetPayrollCalendars", context.Background(), tenantID).Return(payRollCalendarResp, nil)
+		mockClient.On("EmployeeLeaveBalance", context.Background(), tenantID, empID).Return(leaveBalResp, nil)
+		mockClient.On("EmployeeLeaveApplication", context.Background(), tenantID, mock.Anything).Return(nil)
+		xlsLocation := getProjectRoot() + "/test/AllOrg.xlsx"
+		service := NewService(mockClient, xlsLocation)
+
+		errRes := service.MigrateLeaveKrowToXero(context.Background())
+		assert.NotNil(t, errRes)
+		assert.Equal(t, true, contains(errRes, "Failed to get ORG details from Xero. OrgName: Eliiza"))
+		assert.Equal(t, true, contains(errRes, "Failed to get ORG details from Xero. OrgName: CMD"))
+	})
+
+	t.Run("Error when employee does not have the applied leave type configured in Xero", func(t *testing.T) {
+		expectedError := "leave type Personal/Carer's Leave not found in Xero for Employee: Syril Sadasivan"
 
 		empResp := &xero.EmpResponse{
 			Status: "Active",
@@ -56,17 +137,7 @@ func TestLeaveMigration(t *testing.T) {
 					PayrollCalendarID: "4567891011",
 					LeaveBalance: []xero.LeaveBalance{
 						annualLeave,
-						personalLeave,
 					},
-				},
-			},
-		}
-
-		payRollCalendarResp := &xero.PayrollCalendarResponse{
-			PayrollCalendars: []xero.PayrollCalendar{
-				{
-					PayrollCalendarID: "4567891011",
-					PaymentDate:       "/Date(632102400000+0000)/",
 				},
 			},
 		}
@@ -79,12 +150,47 @@ func TestLeaveMigration(t *testing.T) {
 		mockClient.On("GetPayrollCalendars", context.Background(), tenantID).Return(payRollCalendarResp, nil)
 		mockClient.On("EmployeeLeaveBalance", context.Background(), tenantID, empID).Return(leaveBalResp, nil)
 		mockClient.On("EmployeeLeaveApplication", context.Background(), tenantID, mock.Anything).Return(nil)
-		xlsLocation := dir + "/test/digioLeave.xlsx"
+		xlsLocation := getProjectRoot() + "/test/digioLeave.xlsx"
 		service := NewService(mockClient, xlsLocation)
 
-		err := service.MigrateLeaveKrowToXero(context.Background())
-		assert.Nil(t, err)
+		errRes := service.MigrateLeaveKrowToXero(context.Background())
+		assert.NotNil(t, errRes)
+		assert.Equal(t, true, contains(errRes, expectedError))
 	})
+
+	t.Run("Error when employee is missing in Xero", func(t *testing.T) {
+		expectedError := "employee not found in Xero: Stina Anderson"
+
+		mockClient := new(MockXeroClient)
+		mockClient.On("GetConnections", context.Background()).Return(connectionResp, nil)
+		mockClient.On("GetEmployees", context.Background(), tenantID).Return(empResp, nil)
+		mockClient.On("GetPayrollCalendars", context.Background(), tenantID).Return(payRollCalendarResp, nil)
+		mockClient.On("EmployeeLeaveBalance", context.Background(), tenantID, empID).Return(leaveBalResp, nil)
+		mockClient.On("EmployeeLeaveApplication", context.Background(), tenantID, mock.Anything).Return(nil)
+		xlsLocation := getProjectRoot() + "/test/AllOrg.xlsx"
+		service := NewService(mockClient, xlsLocation)
+
+		errRes := service.MigrateLeaveKrowToXero(context.Background())
+		assert.NotNil(t, errRes)
+		assert.Equal(t, 12, len(errRes))
+		assert.Equal(t, true, contains(errRes, expectedError))
+	})
+}
+
+func contains(errors []string, errStr string) bool {
+	for _, s := range errors {
+		if strings.Contains(s, errStr) {
+			return true
+		}
+	}
+	return false
+}
+
+func getProjectRoot() string {
+	_, b, _, _ := runtime.Caller(0)
+	basePath := filepath.Dir(b)
+	dir := path.Join(path.Dir(basePath), ".")
+	return dir
 }
 
 func (m MockXeroClient) GetEmployees(ctx context.Context, tenantID string) (*xero.EmpResponse, error) {
